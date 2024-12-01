68ac3c72a2ad9d5b8db84867e3753bb12bbbfa4f
Dan Snow <danjsnow@outlook.com>
2024-12-01 15:52:55 +0000

    :hammer: Create setup scripts (#4)
    * :recycle: Move project files up a level
    
    Nicer to have the project file at the top level so the extra files like
    the README show up in Rider.
    
    * :bento: Add template for day file
    
    Capital lettered words to be replaced by the setup script.
    
    * :hammer: Script creation of day class
    
    Doesn't add tests yet but will do for now.

---
+++ setup_scripts/DayTemplate.cs
@@ -0,0 +1,27 @@
+namespace AOC.DaysYEAR_NUMBER.CLASS_NAME;
+
+public class CLASS_NAME : Day<string, int, string, int>
+{
+    protected override int DayNumber => DAY_NUMBER;
+    protected override int Year => YEAR_NUMBER;
+
+    protected override string ParseInputPart1(StreamReader input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override int SolvePart1(string parsedInput)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override string ParseInputPart2(StreamReader input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override int SolvePart2(string parsedInput)
+    {
+        throw new NotImplementedException();
+    }
+}
