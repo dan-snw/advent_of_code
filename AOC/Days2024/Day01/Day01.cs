192e34e4ac2d39b0280cc920fbcac2b74ca74c13
Dan Snow <danjsnow@outlook.com>
2024-12-01 16:53:15 +0000

    :sparkles: Solve part 2
    Again without refactoring - not a fan of the nested for each loops but
    it works for now.

--- AOC/Days2024/Day01/Day01.cs
+++ AOC/Days2024/Day01/Day01.cs
@@ -1,6 +1,6 @@
 namespace AOC.Days2024.Day01;
 
-public class Day01 : Day<(List<int>, List<int>), int, string, int>
+public class Day01 : Day<(List<int>, List<int>), int, (List<int>, List<int>), int>
 {
     protected override int DayNumber => 1;
     protected override int Year => 2024;
@@ -33,13 +33,32 @@ public class Day01 : Day<(List<int>, List<int>), int, string, int>
         return sumOfDifferences;
     }
 
-    protected override string ParseInputPart2(StreamReader input)
+    protected override (List<int>, List<int>) ParseInputPart2(StreamReader input)
     {
-        throw new NotImplementedException();
+        return ParseInputPart1(input);
     }
 
-    protected override int SolvePart2(string parsedInput)
+    protected override int SolvePart2((List<int>, List<int>) parsedInput)
     {
-        throw new NotImplementedException();
+        var list1 = parsedInput.Item1;
+        var list2 = parsedInput.Item2;
+        var appearancesInSecondList = new Dictionary<int, int>();
+        var similarityScore = 0;
+        foreach (var i in list1)
+        {
+            if (!appearancesInSecondList.ContainsKey(i))
+            {
+                appearancesInSecondList[i] = 0;
+                foreach (var j in list2)
+                {
+                    if (i == j)
+                    {
+                        appearancesInSecondList[i]++;
+                    }
+                }
+            }
+            similarityScore += appearancesInSecondList[i] * i;
+        }
+        return similarityScore;
     }
 }
