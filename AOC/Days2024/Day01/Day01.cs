6b8b89965b875682b68dda06c2b332e5bdade284
Dan Snow <danjsnow@outlook.com>
2024-12-01 18:32:31 +0000

    :sparkles: Solve day 01, 2024 (#5)
    * :construction: Set up template code
    
    Empty failing tests and nothing implemented yet.
    
    * :test_tube: Add test of part 1
    
    Based on example given in the puzzle description.
    
    Failng because nothing is implemented yet.
    
    * :sparkles: Solve part 1
    
    No refactoring yet - can convert to a nice LINQ expression.
    
    * :test_tube: Add test of part 2
    
    Uses the same example input.
    
    * :sparkles: Solve part 2
    
    Again without refactoring - not a fan of the nested for each loops but
    it works for now.
    
    * :zap: Parse into dictionary once for part 2
    
    No need to parse the input into a list and then a dictionary in part 2 -
    can parse directly into the dictionary when reading from the input.
    
    Also refactors to use LINQ now that the parsing is done beforehand.
    
    * :recycle: Use LINQ in part 1
    
    Can get rid of the foreach loop by using a LINQ select query.
    
    Also put the sorting in the parsing part since it's really part of
    getting the data in shape.

---
+++ AOC/Days2024/Day01/Day01.cs
@@ -0,0 +1,43 @@
+namespace AOC.Days2024.Day01;
+
+public class Day01 : Day<(List<int>, List<int>), int, (List<int>, Dictionary<int, int>), int>
+{
+    protected override int DayNumber => 1;
+    protected override int Year => 2024;
+
+    protected override (List<int>, List<int>) ParseInputPart1(StreamReader input)
+    {
+        var list1 = new List<int>();
+        var list2 = new List<int>();
+
+        while (!input.EndOfStream)
+        {
+            var inputLine = input.ReadLine()?.Split("   ");
+            list1.Add(Convert.ToInt32(inputLine?[0]));
+            list2.Add(Convert.ToInt32(inputLine?[1]));
+        }
+        list1.Sort();
+        list2.Sort();
+        return (list1, list2);
+    }
+
+    protected override int SolvePart1((List<int>, List<int>) parsedInput) =>
+        parsedInput.Item1.Select((t, i) => Math.Abs(t - parsedInput.Item2[i])).Sum();
+
+    protected override (List<int>, Dictionary<int, int>) ParseInputPart2(StreamReader input)
+    {
+        var list1 = new List<int>();
+        var appearancesInSecondList = new Dictionary<int, int>();
+        while (!input.EndOfStream)
+        {
+            var inputLine = input.ReadLine()?.Split("   ");
+            list1.Add(Convert.ToInt32(inputLine?[0]));
+            appearancesInSecondList.TryAdd(Convert.ToInt32(inputLine?[1]), 0);
+            appearancesInSecondList[Convert.ToInt32(inputLine?[1])]++;
+        }
+        return (list1, appearancesInSecondList);
+    }
+
+    protected override int SolvePart2((List<int>, Dictionary<int, int>) parsedInput) =>
+        parsedInput.Item1.Sum(i => i * parsedInput.Item2.GetValueOrDefault(i, 0));
+}
