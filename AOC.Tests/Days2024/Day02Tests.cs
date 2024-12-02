67af902e3de767e2a07e01d006a1bd906d987efc
Dan Snow <danjsnow@outlook.com>
2024-12-02 22:27:57 +0000

    :sparkles: Solve day 02, 2024 (#8)
    * :construction: Add template code for day 2
    
    Empty tests and an empty class.
    
    * :test_tube: Add test for part 1
    
    Currently failing because nothing is implemented.
    
    * :sparkles: Solve part 1
    
    * :test_tube: Add test for part 2
    
    * :poop: Messy version that works
    
    Terrible method calling and hierarchy but gets the job done for now.
    
    * :recycle: Removed overkill class
    
    Really don't need a separate class that just contains a list of ints -
    easier to just pass them into a list of ints and then have some
    extension methods on that.
    
    Still not ideally performant - would be better to stop at the point
    where an error is found, and then try removing that level, the one
    below, and the one above. But that becomes a lot less readable.

---
+++ AOC.Tests/Days2024/Day02Tests.cs
@@ -0,0 +1,39 @@
+using AOC.Days2024.Day02;
+using AOC.Tests.Common;
+using FluentAssertions;
+
+namespace AOC.Tests.Days2024;
+
+public class Day02Tests
+{
+    private const string TestInput =
+        "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9";
+
+    [Fact]
+    public void Day01_ParseAndSolveP1_ReturnCorrectAnswer()
+    {
+        // Arrange
+        Day02 day = new();
+        var testInputStream = MethodsForTesting.StringToStreamReader(TestInput);
+
+        // Act
+        var result = day.ParseAndSolveP1(testInputStream);
+
+        // Assert
+        result.Should().Be(2);
+    }
+
+    [Fact]
+    public void Day01_ParseAndSolveP2_ReturnCorrectAnswer()
+    {
+        // Arrange
+        Day02 day = new();
+        var testInputStream = MethodsForTesting.StringToStreamReader(TestInput);
+
+        // Act
+        var result = day.ParseAndSolveP2(testInputStream);
+
+        // Assert
+        result.Should().Be(4);
+    }
+}
