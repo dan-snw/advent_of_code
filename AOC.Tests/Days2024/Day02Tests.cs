d85e50baf5a42ee38b53f6b7dfe7a36f49e21097
Dan Snow <danjsnow@outlook.com>
2024-12-02 07:43:25 +0000

    :test_tube: Add test for part 1
    Currently failing because nothing is implemented.

--- AOC.Tests/Days2024/Day02Tests.cs
+++ AOC.Tests/Days2024/Day02Tests.cs
@@ -6,7 +6,8 @@ namespace AOC.Tests.Days2024;
 
 public class Day02Tests
 {
-    private const string TestInput = "";
+    private const string TestInput =
+        "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9";
 
     [Fact]
     public void Day01_ParseAndSolveP1_ReturnCorrectAnswer()
@@ -19,7 +20,7 @@ public class Day02Tests
         var result = day.ParseAndSolveP1(testInputStream);
 
         // Assert
-        result.Should().Be(0);
+        result.Should().Be(2);
     }
 
     [Fact]
