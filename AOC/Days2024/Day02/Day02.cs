5ee43a263d6d353fd22f90124f2708c830d828d7
Dan Snow <danjsnow@outlook.com>
2024-12-02 20:25:58 +0000

    :recycle: Removed overkill class
    Really don't need a separate class that just contains a list of ints -
    easier to just pass them into a list of ints and then have some
    extension methods on that.
    
    Still not ideally performant - would be better to stop at the point
    where an error is found, and then try removing that level, the one
    below, and the one above. But that becomes a lot less readable.

--- AOC/Days2024/Day02/Day02.cs
+++ AOC/Days2024/Day02/Day02.cs
@@ -1,23 +1,58 @@
 namespace AOC.Days2024.Day02;
 
-public class Day02 : Day<List<Report>, int, List<Report>, int>
+public class Day02 : Day<List<List<int>>, int, List<List<int>>, int>
 {
     protected override int DayNumber => 2;
     protected override int Year => 2024;
 
-    protected override List<Report> ParseInputPart1(StreamReader input)
+    protected override List<List<int>> ParseInputPart1(StreamReader input)
     {
-        var records = new List<Report>();
+        var reports = new List<List<int>>();
         while (!input.EndOfStream)
         {
-            records.Add(new(input.ReadLine()!.Split(" ").Select(int.Parse).ToList()));
+            reports.Add([.. input.ReadLine()!.Split(" ").Select(int.Parse).ToList()]);
         }
-        return records;
+        return reports;
     }
 
-    protected override int SolvePart1(List<Report> reports) => reports.Count(x => x.IsSafe());
+    protected override int SolvePart1(List<List<int>> reports) => reports.Count(x => x.IsSafe());
 
-    protected override List<Report> ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+    protected override List<List<int>> ParseInputPart2(StreamReader input) =>
+        ParseInputPart1(input);
 
-    protected override int SolvePart2(List<Report> reports) => reports.Count(x => x.IsSafe(true));
+    protected override int SolvePart2(List<List<int>> reports) =>
+        reports.Count(x => x.IsSafeWithDampener());
+}
+
+internal static class ListExtensions
+{
+    public static bool IsSafeWithDampener(this List<int> report)
+    {
+        for (var i = 0; i < report.Count; i++)
+        {
+            var newList = new List<int>(report);
+            newList.RemoveAt(i);
+            if (newList.IsSafe())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static bool IsSafe(this List<int> report)
+    {
+        var isAscending = report[0] < report[1];
+        for (var i = 0; i < report.Count - 1; i++)
+        {
+            if (!IsSafe(report[i], report[i + 1], isAscending))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static bool IsSafe(int l1, int l2, bool isAscending) =>
+        Math.Abs(l1 - l2) is >= 1 and <= 3 && l1 < l2 == isAscending;
 }
