b0560994af35bdb21dc0ac60fc613202321db7ad
Dan Snow <dan.snow@fundapps.co>
2024-12-03 17:49:16 +0000

    :sparkles: Solve part 1

--- AOC/Days2024/Day03/Day03.cs
+++ AOC/Days2024/Day03/Day03.cs
@@ -1,21 +1,28 @@
+using System.Text.RegularExpressions;
+
 namespace AOC.Days2024.Day03;
 
-public class Day03 : Day<string, int, string, int>
+public class Day03 : Day<List<(int, int)>, int, List<(int, int)>, int>
 {
     protected override int DayNumber => 3;
     protected override int Year => 2024;
 
-    protected override string ParseInputPart1(StreamReader input)
+    protected override List<(int, int)>  ParseInputPart1(StreamReader input)
     {
-        throw new NotImplementedException();
+        var rawInputLine = "";
+        while (!input.EndOfStream)
+        {
+            rawInputLine += input.ReadLine();
+        }
+        const string regexPattern = @"mul\([0-9]{1,3},[0-9]{1,3}\)";
+        var cleanInput = Regex.Matches(rawInputLine!, regexPattern).Select(x => x.ToString().Replace("mul(", "").Replace(")", ""));
+        var splitInput = cleanInput.Select(x => x.Split(","));
+        return splitInput.Select(array => (int.Parse(array[0]), int.Parse(array[1]))).ToList();
     }
 
-    protected override int SolvePart1(string parsedInput)
-    {
-        throw new NotImplementedException();
-    }
+    protected override int SolvePart1(List<(int, int)> multiplicationPairs) => multiplicationPairs.Sum(pair => pair.Item1 * pair.Item2);
 
-    protected override string ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+    protected override List<(int, int)> ParseInputPart2(StreamReader input) => ParseInputPart1(input);
 
-    protected override int SolvePart2(string parsedInput) => SolvePart1(parsedInput);
+    protected override int SolvePart2(List<(int, int)> parsedInput) => SolvePart1(parsedInput);
 }
