cfe825f921d1dcbef090e0914ce45a206e5b6b9d
Dan Snow <dan.snow@fundapps.co>
2024-12-03 17:50:58 +0000

    :test_tube: Add test for part 2

--- AOC.Tests/Days2024/Day03Tests.cs
+++ AOC.Tests/Days2024/Day03Tests.cs
@@ -6,14 +6,14 @@ namespace AOC.Tests.Days2024;
 
 public class Day03Tests
 {
-    private const string TestInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
     [Fact]
     public void Day03_ParseAndSolveP1_ReturnCorrectAnswer()
     {
         // Arrange
+        const string testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         Day03 day = new();
-        var testInputStream = MethodsForTesting.StringToStreamReader(TestInput);
+        var testInputStream = MethodsForTesting.StringToStreamReader(testInput);
 
         // Act
         var result = day.ParseAndSolveP1(testInputStream);
@@ -26,13 +26,14 @@ public class Day03Tests
     public void Day03_ParseAndSolveP2_ReturnCorrectAnswer()
     {
         // Arrange
+        const string testInput = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
         Day03 day = new();
-        var testInputStream = MethodsForTesting.StringToStreamReader(TestInput);
+        var testInputStream = MethodsForTesting.StringToStreamReader(testInput);
 
         // Act
         var result = day.ParseAndSolveP2(testInputStream);
 
         // Assert
-        result.Should().Be(0);
+        result.Should().Be(48);
     }
 }
