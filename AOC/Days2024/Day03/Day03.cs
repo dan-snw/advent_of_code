f6c5f52d83b5771f2f0a99ab388a8e799594316b
Dan Snow <dan.snow@fundapps.co>
2024-12-03 18:00:48 +0000

    :sparkles: Solve part 2
    Messy but does the job - can refactor later.

--- AOC/Days2024/Day03/Day03.cs
+++ AOC/Days2024/Day03/Day03.cs
@@ -2,7 +2,7 @@ using System.Text.RegularExpressions;
 
 namespace AOC.Days2024.Day03;
 
-public class Day03 : Day<List<(int, int)>, int, List<(int, int)>, int>
+public class Day03 : Day<List<(int, int)>, int, List<string>, int>
 {
     protected override int DayNumber => 3;
     protected override int Year => 2024;
@@ -22,7 +22,38 @@ public class Day03 : Day<List<(int, int)>, int, List<(int, int)>, int>
 
     protected override int SolvePart1(List<(int, int)> multiplicationPairs) => multiplicationPairs.Sum(pair => pair.Item1 * pair.Item2);
 
-    protected override List<(int, int)> ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+    protected override List<string> ParseInputPart2(StreamReader input)
+    {
+        const string regexPattern = @"mul\([0-9]{1,3},[0-9]{1,3}\)|don't\(\)|do\(\)";
+        var rawInputLine = "";
+        while (!input.EndOfStream)
+        {
+            rawInputLine += input.ReadLine();
+        }
+        var cleanInput = Regex.Matches(rawInputLine!, regexPattern).Select(x => x.ToString());
+        return cleanInput.ToList();
+    }
 
-    protected override int SolvePart2(List<(int, int)> parsedInput) => SolvePart1(parsedInput);
+    protected override int SolvePart2(List<string> parsedInput)
+    {
+        var enabled = true;
+        var total = 0;
+        foreach (var line in parsedInput)
+        {
+            if (line.StartsWith("mul") & enabled)
+            {
+                var multipliers = line.Replace("mul(", "").Replace(")", "").Split(",");
+                total += int.Parse(multipliers[0]) * int.Parse(multipliers[1]);
+            }
+            if (line.StartsWith("do("))
+            {
+                enabled = true;
+            }
+            if (line.StartsWith("don't"))
+            {
+                enabled = false;
+            }
+        }
+        return total;
+    }
 }
