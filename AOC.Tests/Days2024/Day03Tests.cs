d4dc04a84a8862c740fd16b3e57328199453f5de
Dan Snow <dan.snow@fundapps.co>
2024-12-03 08:55:51 +0000

    :test_tube: Add test for part 1

--- AOC.Tests/Days2024/Day03Tests.cs
+++ AOC.Tests/Days2024/Day03Tests.cs
@@ -6,7 +6,7 @@ namespace AOC.Tests.Days2024;
 
 public class Day03Tests
 {
-    private const string TestInput = "";
+    private const string TestInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
     [Fact]
     public void Day03_ParseAndSolveP1_ReturnCorrectAnswer()
@@ -19,7 +19,7 @@ public class Day03Tests
         var result = day.ParseAndSolveP1(testInputStream);
 
         // Assert
-        result.Should().Be(0);
+        result.Should().Be(161);
     }
 
     [Fact]
