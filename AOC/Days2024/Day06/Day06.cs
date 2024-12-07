8a348c79b19eb087947aec8f7453d9a6e9a168fd
Dan Snow <danjsnow@outlook.com>
2024-12-07 18:32:04 +0000

    :construction: Generate template code

---
+++ AOC/Days2024/Day06/Day06.cs
@@ -0,0 +1,21 @@
+namespace AOC.Days2024.Day06;
+
+public class Day06 : Day<string, int, string, int>
+{
+    protected override int DayNumber => 6;
+    protected override int Year => 2024;
+
+    protected override string ParseInputPart1(StreamReader input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override int SolvePart1(string parsedInput)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override string ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+
+    protected override int SolvePart2(string parsedInput) => SolvePart1(parsedInput);
+}
