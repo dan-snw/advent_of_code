d5d1f0e483bb9a86307fd46eff2e654f09e0021d
Dan Snow <danjsnow@outlook.com>
2024-12-08 10:37:51 +0000

    :recycle: Create map class
    It's going to start getting messy in part 2 so might as well put it
    into a separate class.

--- AOC/Days2024/Day06/Day06.cs
+++ AOC/Days2024/Day06/Day06.cs
@@ -2,24 +2,16 @@ using AOC.Common;
 
 namespace AOC.Days2024.Day06;
 
-public class Day06
-    : Day<
-        (Dictionary<Coordinate, bool>, Coordinate),
-        int,
-        (Dictionary<Coordinate, bool>, Coordinate),
-        int
-    >
+public class Day06 : Day<Map, int, Map, int>
 {
     protected override int DayNumber => 6;
     protected override int Year => 2024;
 
-    protected override (Dictionary<Coordinate, bool>, Coordinate) ParseInputPart1(
-        StreamReader input
-    )
+    protected override Map ParseInputPart1(StreamReader input)
     {
         var lineNumber = 0;
+        var startingPosition = new Coordinate(0, 0);
         var obstacleMap = new Dictionary<Coordinate, bool>();
-        var startingCoordinate = new Coordinate(0, 0);
         while (!input.EndOfStream)
         {
             var line = input.ReadLine();
@@ -27,37 +19,21 @@ public class Day06
             {
                 if (line[i] == '^')
                 {
-                    startingCoordinate = new(i, lineNumber);
+                    startingPosition = new(i, lineNumber);
                 }
                 obstacleMap.Add(new(i, lineNumber), line[i] == '#');
             }
             lineNumber++;
         }
-        return (obstacleMap, startingCoordinate);
+        return new Map(obstacleMap, startingPosition);
     }
 
-    protected override int SolvePart1((Dictionary<Coordinate, bool>, Coordinate) parsedInput)
+    protected override int SolvePart1(Map map) => map.GetVisitedPositions().Count;
+
+    protected override Map ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+
+    protected override int SolvePart2(Map map)
     {
-        var map = parsedInput.Item1;
-        var position = parsedInput.Item2;
-        var direction = CompassPoint.North;
-        var visited = new HashSet<Coordinate> { position };
-        while (map.ContainsKey(position.GetNext(direction)))
-        {
-            if (map[position.GetNext(direction)])
-            {
-                direction = Coordinate.RotateDirection90(direction);
-            }
-            position = position.GetNext(direction);
-            visited.Add(position);
-        }
-        return visited.Count;
+        return SolvePart1(map);
     }
-
-    protected override (Dictionary<Coordinate, bool>, Coordinate) ParseInputPart2(
-        StreamReader input
-    ) => ParseInputPart1(input);
-
-    protected override int SolvePart2((Dictionary<Coordinate, bool>, Coordinate) parsedInput) =>
-        SolvePart1(parsedInput);
 }
