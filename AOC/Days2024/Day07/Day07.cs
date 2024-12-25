0ed617b5fdc248b17356aa577a7426894d091b11
Dan Snow <danjsnow@outlook.com>
2024-12-25 09:13:18 +0000

    :recycle: Tidy into methods a bit
    Not perfect with the for loop logic but at least has a few methods to
    break things out.

--- AOC/Days2024/Day07/Day07.cs
+++ AOC/Days2024/Day07/Day07.cs
@@ -18,35 +18,41 @@ public class Day07 : Day<List<(long, List<long>)>, long, List<(long, List<long>)
         return parsedInput;
     }
 
-    protected override long SolvePart1(List<(long, List<long>)> parsedInput)
-    {
-        long sum = 0;
-        foreach (var line in parsedInput)
-        {
-            var combinations = GetCombinations(line.Item2.Count - 1, ["+", "*"]);
-            foreach (var combination in combinations)
-            {
-                var total = line.Item2[0];
-                for (var i = 1; i < line.Item2.Count; i++)
-                {
-                    total =
-                        combination[i - 1] == "+" ? total + line.Item2[i] : total * line.Item2[i];
-                }
-                if (total == line.Item1)
-                {
-                    sum += line.Item1;
-                    break;
-                }
-            }
-        }
-        return sum;
-    }
+    protected override long SolvePart1(List<(long, List<long>)> parsedInput) =>
+        parsedInput.Where(l => CanBeMadeTrue(l, ["+", "*"])).Sum(line => line.Item1);
 
     protected override List<(long, List<long>)> ParseInputPart2(StreamReader input) =>
         ParseInputPart1(input);
 
     protected override long SolvePart2(List<(long, List<long>)> parsedInput) =>
         SolvePart1(parsedInput);
+
+    private static bool CanBeMadeTrue((long, List<long>) line, string[] characters)
+    {
+        var combinations = GetCombinations(line.Item2.Count - 1, characters);
+        foreach (var combination in combinations)
+        {
+            var total = line.Item2[0];
+            for (var i = 1; i < line.Item2.Count; i++)
+            {
+                total = ApplyOperation(total, line.Item2[i], combination[i - 1]);
+            }
+            if (total == line.Item1)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static long ApplyOperation(long value1, long value2, string character) =>
+        character switch
+        {
+            "+" => value1 + value2,
+            "*" => value1 * value2,
+            _ => throw new NotImplementedException(),
+        };
 
     public static List<string[]> GetCombinations(int length, string[] characters)
     {
