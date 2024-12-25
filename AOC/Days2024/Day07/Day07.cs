c77204e7465bccf8eae292a55e243f1c96f9b9f1
Dan Snow <danjsnow@outlook.com>
2024-12-25 08:58:29 +0000

    :sparkles: Solve part 1
    Had to switch to using longs because the numbers got too big.

--- AOC/Days2024/Day07/Day07.cs
+++ AOC/Days2024/Day07/Day07.cs
@@ -1,32 +1,51 @@
 namespace AOC.Days2024.Day07;
 
-public class Day07 : Day<List<(int, List<int>)>, int, List<(int, List<int>)>, int>
+public class Day07 : Day<List<(long, List<long>)>, long, List<(long, List<long>)>, long>
 {
     protected override int DayNumber => 7;
     protected override int Year => 2024;
 
-    public override List<(int, List<int>)> ParseInputPart1(StreamReader input)
+    public override List<(long, List<long>)> ParseInputPart1(StreamReader input)
     {
-        var parsedInput = new List<(int, List<int>)>();
+        var parsedInput = new List<(long, List<long>)>();
         while (!input.EndOfStream)
         {
             var split = input.ReadLine()!.Split(": ");
             parsedInput.Add(
-                (int.Parse(split.First()), split[1].Split(" ").Select(int.Parse).ToList())
+                (long.Parse(split.First()), split[1].Split(" ").Select(long.Parse).ToList())
             );
         }
         return parsedInput;
     }
 
-    protected override int SolvePart1(List<(int, List<int>)> parsedInput)
+    protected override long SolvePart1(List<(long, List<long>)> parsedInput)
     {
-        throw new NotImplementedException();
+        long sum = 0;
+        foreach (var line in parsedInput)
+        {
+            var combinations = GetCombinations(line.Item2.Count - 1, ["+", "*"]);
+            foreach (var combination in combinations)
+            {
+                var total = line.Item2[0];
+                for (var i = 1; i < line.Item2.Count; i++)
+                {
+                    total =
+                        combination[i - 1] == "+" ? total + line.Item2[i] : total * line.Item2[i];
+                }
+                if (total == line.Item1)
+                {
+                    sum += line.Item1;
+                    break;
+                }
+            }
+        }
+        return sum;
     }
 
-    protected override List<(int, List<int>)> ParseInputPart2(StreamReader input) =>
+    protected override List<(long, List<long>)> ParseInputPart2(StreamReader input) =>
         ParseInputPart1(input);
 
-    protected override int SolvePart2(List<(int, List<int>)> parsedInput) =>
+    protected override long SolvePart2(List<(long, List<long>)> parsedInput) =>
         SolvePart1(parsedInput);
 
     public static List<string[]> GetCombinations(int length, string[] characters)
