19f28a700cdac4bff88214202827eb86c40dac82
Dan Snow <danjsnow@outlook.com>
2024-12-31 10:38:03 +0000

    :construction: Parse input

--- AOC/Days2024/Day09/Day09.cs
+++ AOC/Days2024/Day09/Day09.cs
@@ -1,21 +1,37 @@
 namespace AOC.Days2024.Day09;
 
-public class Day09 : Day<string, int, string, int>
+public class Day09 : Day<List<string>, int, List<string>, int>
 {
     protected override int DayNumber => 9;
     protected override int Year => 2024;
 
-    public override string ParseInputPart1(StreamReader input)
+    public override List<string> ParseInputPart1(StreamReader input)
+    {
+        var compressed = input.ReadLine()!.Select(s => int.Parse(s.ToString())).ToList();
+        var gap = false;
+        var uncompressed = new List<string>();
+        var index = 0;
+        foreach (var block in compressed)
+        {
+            for (var j = 0; j < block; j++)
+            {
+                uncompressed.Add(gap ? "." : $"{index}");
+            }
+            gap = !gap;
+            if (gap)
+            {
+                index++;
+            }
+        }
+        return uncompressed;
+    }
+
+    protected override int SolvePart1(List<string> parsedInput)
     {
         throw new NotImplementedException();
     }
 
-    protected override int SolvePart1(string parsedInput)
-    {
-        throw new NotImplementedException();
-    }
+    protected override List<string> ParseInputPart2(StreamReader input) => ParseInputPart1(input);
 
-    protected override string ParseInputPart2(StreamReader input) => ParseInputPart1(input);
-
-    protected override int SolvePart2(string parsedInput) => SolvePart1(parsedInput);
+    protected override int SolvePart2(List<string> parsedInput) => SolvePart1(parsedInput);
 }
