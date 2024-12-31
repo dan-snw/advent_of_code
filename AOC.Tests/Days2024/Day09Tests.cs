c7a6bbf66f627629ebe4a4ab4904fa6a48bef443
Dan Snow <danjsnow@outlook.com>
2024-12-31 09:50:19 +0000

    :construction: Add template code

---
+++ AOC.Tests/Days2024/Day09Tests.cs
@@ -0,0 +1,38 @@
+using AOC.Days2024.Day09;
+using AOC.Tests.Common;
+using FluentAssertions;
+
+namespace AOC.Tests.Days2024;
+
+public class Day09Tests
+{
+    private const string TestInput = "";
+
+    [Fact]
+    public void Day09_ParseAndSolveP1_ReturnCorrectAnswer()
+    {
+        // Arrange
+        Day09 day = new();
+        var testInputStream = MethodsForTesting.StringToStreamReader(TestInput);
+
+        // Act
+        var result = day.ParseAndSolveP1(testInputStream);
+
+        // Assert
+        result.Should().Be(0);
+    }
+
+    [Fact]
+    public void Day09_ParseAndSolveP2_ReturnCorrectAnswer()
+    {
+        // Arrange
+        Day09 day = new();
+        var testInputStream = MethodsForTesting.StringToStreamReader(TestInput);
+
+        // Act
+        var result = day.ParseAndSolveP2(testInputStream);
+
+        // Assert
+        result.Should().Be(0);
+    }
+}
---
+++ AOC/Days2024/Day09/Day09.cs
@@ -0,0 +1,21 @@
+namespace AOC.Days2024.Day09;
+
+public class Day09 : Day<string, int, string, int>
+{
+    protected override int DayNumber => 9;
+    protected override int Year => 2024;
+
+    public override string ParseInputPart1(StreamReader input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override int SolvePart1(string parsedInput)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override string ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+
+    protected override int SolvePart2(string parsedInput) => SolvePart1(parsedInput);
+}
