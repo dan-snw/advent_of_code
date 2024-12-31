06196a3c8f139d1d3295121e8f6c2c24031eba4f
Dan Snow <danjsnow@outlook.com>
2024-12-31 10:36:12 +0000

    :test_tube: Add test for parsing input

--- AOC.Tests/Days2024/Day09Tests.cs
+++ AOC.Tests/Days2024/Day09Tests.cs
@@ -9,6 +9,23 @@ public class Day09Tests
     private const string TestInput = "2333133121414131402";
 
     [Fact]
+    public void Day09_ParseInputPart1_ParsesToList()
+    {
+        // Arrange
+        Day09 day = new();
+        var testInputStream = MethodsForTesting.StringToStreamReader(TestInput);
+
+        // Act
+        var result = day.ParseInputPart1(testInputStream);
+        var expected = "00...111...2...333.44.5555.6666.777.888899"
+            .ToList()
+            .Select(s => s.ToString());
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
     public void Day09_ParseAndSolveP1_ReturnCorrectAnswer()
     {
         // Arrange
