98b7d70dc0acf439cc2e1ac6415de617b9134e4c
Dan Snow <dan.snow@fundapps.co>
2025-12-01 23:42:59 +0000

    :recycle: Get rid of left/right

--- AOC/Days2025/Day01/Day01.cs
+++ AOC/Days2025/Day01/Day01.cs
@@ -1,31 +1,28 @@
 namespace AOC.Days2025.Day01;
 
-public class Day01 : Day<List<Day01.Turn>, int, List<Day01.Turn>, int>
+public class Day01 : Day<List<int>, int, List<int>, int>
 {
     protected override int DayNumber => 1;
     protected override int Year => 2025;
 
-    public override List<Turn> ParseInputPart1(StreamReader input)
+    public override List<int> ParseInputPart1(StreamReader input)
     {
-        List<Turn> turns = [];
+        List<int> turns = [];
         while (!input.EndOfStream)
         {
             var inputLine = input.ReadLine();
-            var direction = inputLine![0] == 'L' ? LeftRight.Left : LeftRight.Right;
-            var amount = int.Parse(inputLine[1..]);
-            turns.Add(new Turn(direction, amount));
+            turns.Add(inputLine![0] == 'L' ? -int.Parse(inputLine[1..]) : int.Parse(inputLine[1..]));
         }
-
         return turns;
     }
 
-    protected override int SolvePart1(List<Turn> parsedInput)
+    protected override int SolvePart1(List<int> parsedInput)
     {
         var timesOnZero = 0;
         var currentPosition = 50;
         foreach (var turn in parsedInput)
         {
-            currentPosition = GetNextPosition(currentPosition, turn);
+            currentPosition = GetNextPosition(currentPosition, turn % 100);
             if (currentPosition == 0)
             {
                 timesOnZero++;
@@ -34,50 +31,33 @@ public class Day01 : Day<List<Day01.Turn>, int, List<Day01.Turn>, int>
         return timesOnZero;
     }
 
-    protected override List<Turn> ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+    protected override List<int> ParseInputPart2(StreamReader input) => ParseInputPart1(input);
 
-    protected override int SolvePart2(List<Turn> parsedInput)
+    protected override int SolvePart2(List<int> parsedInput)
     {
         var timesPassingThroughZero = 0;
         var currentPosition = 50;
         foreach (var turn in parsedInput)
         {
-            timesPassingThroughZero += TimesPassingThroughZero(currentPosition, turn);
-            currentPosition = GetNextPosition(currentPosition, turn);
+            timesPassingThroughZero += Math.Abs(turn / 100);
+            if (currentPosition != 0 && (turn % 100 + currentPosition is <= 0 or > 99 || turn % 100 + currentPosition is 0))
+            {
+                timesPassingThroughZero++;
+            }
+
+            currentPosition = GetNextPosition(currentPosition, turn % 100);
         }
         return timesPassingThroughZero;
     }
 
-    private static int GetNextPosition(int currentPosition, Turn turn)
+    private static int GetNextPosition(int currentPosition, int turn)
     {
-        var amountToTurnRight = turn.Direction == LeftRight.Right ? turn.Amount % 100 : 100 - turn.Amount % 100;
-        return currentPosition + amountToTurnRight <= 99
-            ? currentPosition + amountToTurnRight
-            : currentPosition - (100 - amountToTurnRight);
-    }
-    
-    private static int TimesPassingThroughZero(int currentPosition, Turn turn)
-    {
-        var times = turn.Amount / 100;
-        var nextPosition = GetNextPosition(currentPosition, turn);
-        if (currentPosition == 0 && turn.Amount != 100)
+        var nextPosition = currentPosition + turn;
+        return nextPosition switch
         {
-            return times;
-        }
-        if((nextPosition == 0 && currentPosition != 0)
-           || (turn.Direction == LeftRight.Left && nextPosition >= currentPosition)
-           || (turn.Direction == LeftRight.Right && nextPosition <= currentPosition))
-        {
-            return times + 1;
-        }
-        return times;
-    }
-    
-    public record Turn(LeftRight Direction, int Amount);
-
-    public enum LeftRight
-    {
-        Left,
-        Right
+            > 99 => turn + currentPosition - 100,
+            < 0 => 100 + turn + currentPosition,
+            _ => turn + currentPosition
+        };
     }
 }
