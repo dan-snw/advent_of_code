5e8bba951018c21c789e5b1313df9b5d43afe1cc
Dan Snow <dan.snow@fundapps.co>
2025-12-02 19:45:33 +0000

    :test_tube: Add example to test part 1

--- AOC.Tests/Days2025/Day02Tests.cs
+++ AOC.Tests/Days2025/Day02Tests.cs
@@ -6,7 +6,9 @@ namespace AOC.Tests.Days2025;
 
 public class Day02Tests
 {
-    private const string TestInput = "";
+    private const string TestInput = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224," + 
+                                     "1698522-1698528,446443-446449,38593856-38593862,565653-565659," + 
+                                     "824824821-824824827,2121212118-2121212124";
 
     [Fact]
     public void Day02_ParseAndSolveP1_ReturnCorrectAnswer()
@@ -19,7 +21,7 @@ public class Day02Tests
         var result = day.ParseAndSolveP1(testInputStream);
 
         // Assert
-        result.Should().Be(0);
+        result.Should().Be(1227775554);
     }
 
     [Fact]
