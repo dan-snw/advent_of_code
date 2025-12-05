7cba714d88e99226533efcb1e4be779070bf15d4
Dan Snow <dan.snow@fundapps.co>
2025-12-05 11:24:06 +0000

    :sparkles: Solve part 1

--- AOC/Days2025/Day04/Day04.cs
+++ AOC/Days2025/Day04/Day04.cs
@@ -1,21 +1,55 @@
+using AOC.Common;
+
 namespace AOC.Days2025.Day04;
 
-public class Day04 : Day<string, int, string, int>
+public class Day04 : Day<Dictionary<Coordinate, bool>, int, Dictionary<Coordinate, bool>, int>
 {
     protected override int DayNumber => 4;
     protected override int Year => 2025;
 
-    public override string ParseInputPart1(StreamReader input)
+    public override Dictionary<Coordinate, bool> ParseInputPart1(StreamReader input)
     {
-        throw new NotImplementedException();
+        var map = new Dictionary<Coordinate, bool>();
+        var y = 0;
+        while (!input.EndOfStream)
+        {
+            var line = input.ReadLine()!;
+            for (var x = 0; x < line.Length; x++)
+            {
+                map.Add(new Coordinate(x, y), line[x] == '@');
+            }
+            y++;
+        }
+        return map;
     }
 
-    protected override int SolvePart1(string parsedInput)
+    protected override int SolvePart1(Dictionary<Coordinate, bool> parsedInput)
     {
-        throw new NotImplementedException();
+        var total = 0;
+        foreach (var coordinate in parsedInput.Keys)
+        {
+            if (parsedInput[coordinate])
+            {
+                var surrounding = coordinate.GetSurrounding();
+                var sum = 0;
+                foreach (var surroundingCoordinate in surrounding)
+                {
+                    if (parsedInput.TryGetValue(surroundingCoordinate, out var value) && value)
+                    {
+                        sum++;
+                    }
+                }
+
+                if (sum < 4)
+                {
+                    total++;
+                }
+            }
+        }
+        return total;
     }
 
-    protected override string ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+    protected override Dictionary<Coordinate, bool> ParseInputPart2(StreamReader input) => ParseInputPart1(input);
 
-    protected override int SolvePart2(string parsedInput) => SolvePart1(parsedInput);
+    protected override int SolvePart2(Dictionary<Coordinate, bool> parsedInput) => SolvePart1(parsedInput);
 }
