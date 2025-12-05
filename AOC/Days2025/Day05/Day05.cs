8bcf195e3cec012a8fbc6e50476595a0a7cc0029
Dan Snow <dan.snow@fundapps.co>
2025-12-05 21:58:38 +0000

    :sparkles: Solve part 2 for real

--- AOC/Days2025/Day05/Day05.cs
+++ AOC/Days2025/Day05/Day05.cs
@@ -1,6 +1,6 @@
 namespace AOC.Days2025.Day05;
 
-public class Day05 : Day<Inventory, int, Inventory, long>
+public class Day05 : Day<Inventory, int, HashSet<(ulong Lower, ulong Upper)>, ulong>
 {
     protected override int DayNumber => 5;
     protected override int Year => 2025;
@@ -16,14 +16,14 @@ public class Day05 : Day<Inventory, int, Inventory, long>
             {
                 break;
             }
-            var ranges = line!.Split('-').Select(long.Parse).ToList()!;
+            var ranges = line!.Split('-').Select(ulong.Parse).ToList()!;
             inventory.Ranges.Add((ranges[0], ranges[1]));
         }
         
         while (!input.EndOfStream)
         {
             var line = input.ReadLine()!;
-            inventory.Ingredients.Add(long.Parse(line)); 
+            inventory.Ingredients.Add(ulong.Parse(line)); 
         }
         return inventory;
     }
@@ -33,22 +33,95 @@ public class Day05 : Day<Inventory, int, Inventory, long>
             InRange(ingredient, range.Lower, range.Upper) 
             ));
 
-    private static bool InRange(long number, long lower, long upper) => number >= lower && number <= upper;
-    
-    protected override Inventory ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+    private static bool InRange(ulong number, ulong lower, ulong upper) => number >= lower && number <= upper;
 
-    protected override long SolvePart2(Inventory inventory)
+    protected override HashSet<(ulong Lower, ulong Upper)> ParseInputPart2(StreamReader input)
     {
-        var freshIngredients = new HashSet<long>();
-        foreach (var range in inventory.Ranges)
+        var ranges = new HashSet<(ulong Lower, ulong Upper)>();
+        while (true)
         {
-            for(var i =  range.Lower; i <= range.Upper; i++)
+            var line = input.ReadLine()!;
+            if (!line.Contains('-'))
             {
-                freshIngredients.Add(i);
+                break;
+            }
+            var upperLower = line!.Split('-').Select(ulong.Parse).ToList()!;
+            var range = (upperLower[0], upperLower[1]);    
+            ranges = AddNewRange(ranges, range);
+        }
+        return ranges;
+    }
+
+    private HashSet<(ulong Lower, ulong Upper)> AddNewRange(HashSet<(ulong Lower, ulong Upper)> inventoryRanges, (ulong, ulong) newRange)
+    {
+        var overlappingRanges = new HashSet<(ulong Lower, ulong Upper)>();
+        foreach (var range in inventoryRanges.ToList())
+        {
+            if (Overlap(range, newRange))
+            {
+                overlappingRanges.Add(range);
             }
         }
-        return freshIngredients.Count;
+
+        foreach (var range in overlappingRanges)
+        {
+            inventoryRanges.Remove(range);
+        }
+        
+        if (overlappingRanges.Count == 0)
+        {
+            inventoryRanges.Add(newRange);
+        }
+        else
+        {
+            overlappingRanges.Add(newRange);
+            inventoryRanges.Add(GetOverlap(overlappingRanges));
+        }
+        
+        return inventoryRanges;
     }
+
+    private (ulong Lower, ulong Upper) GetOverlap(HashSet<(ulong Lower, ulong Upper)> overlappingRanges)
+    {
+        var lower = ulong.MaxValue;
+        var upper = ulong.MinValue;
+        foreach (var range in overlappingRanges)
+        {
+            if (range.Lower < lower)
+            {
+                lower = range.Lower;
+            }
+            if (range.Upper > upper)
+            {
+                upper = range.Upper;
+            }
+        }
+        return (lower, upper);
+    }
+
+
+    protected override ulong SolvePart2(HashSet<(ulong Lower, ulong Upper)> ranges)
+    {
+        var total = (ulong)0;
+        foreach (var range in ranges)
+        {
+            total += range.Upper - range.Lower + 1;
+        }
+
+        return total;
+    }
+    
+    private bool Overlap((ulong Lower, ulong Upper) first, (ulong Lower, ulong Upper) second) =>
+        InRange(first.Lower, second) 
+        || InRange(first.Upper, second)
+        || InRange(second.Lower, first)
+        || InRange(second.Upper, first);
+    
+    private bool InRange(ulong number, (ulong Lower, ulong Upper) second) =>
+        number >= second.Lower && number <= second.Upper;
+    
+    private (ulong Lower, ulong Upper) CreateOverlap((ulong Lower, ulong Upper) first, (ulong Lower, ulong Upper) second) =>
+        (Math.Min(first.Lower, second.Lower), Math.Max(first.Upper, second.Upper));
 }
 
-public record Inventory(HashSet<(long Lower, long Upper)> Ranges, HashSet<long> Ingredients);
+public record Inventory(HashSet<(ulong Lower, ulong Upper)> Ranges, HashSet<ulong> Ingredients);
