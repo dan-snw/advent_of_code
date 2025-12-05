e2066f6b25cd74f4acef0c62d3a5f5a41b7ccb56
Dan Snow <dan.snow@fundapps.co>
2025-12-05 13:44:42 +0000

    :test_tube: Test part 1

--- AOC.Tests/Days2025/Day05Tests.cs
+++ AOC.Tests/Days2025/Day05Tests.cs
@@ -6,7 +6,18 @@ namespace AOC.Tests.Days2025;
 
 public class Day05Tests
 {
-    private const string TestInput = "";
+    private const string TestInput =
+        "3-5\n" +
+        "10-14\n" +
+        "16-20\n" +
+        "12-18\n" +
+
+        "1\n" +
+        "5\n" +
+        "8\n" +
+        "11\n" +
+        "17\n" +
+        "32\n";
 
     [Fact]
     public void Day05_ParseAndSolveP1_ReturnCorrectAnswer()
@@ -19,7 +30,7 @@ public class Day05Tests
         var result = day.ParseAndSolveP1(testInputStream);
 
         // Assert
-        result.Should().Be(0);
+        result.Should().Be(3);
     }
 
     [Fact]
