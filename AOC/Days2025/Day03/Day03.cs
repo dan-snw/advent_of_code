14d7a785ed53ca8b798c01bdc97b4d907f74d715
Dan Snow <dan.snow@fundapps.co>
2025-12-05 10:02:54 +0000

    :recycle: Parse into int list

--- AOC/Days2025/Day03/Day03.cs
+++ AOC/Days2025/Day03/Day03.cs
@@ -1,38 +1,40 @@
 namespace AOC.Days2025.Day03;
 
-public class Day03 : Day<List<string>, int, List<string>, int>
+public class Day03 : Day<List<List<int>>, int, List<List<int>>, int>
 {
     protected override int DayNumber => 3;
     protected override int Year => 2025;
 
-    public override List<string> ParseInputPart1(StreamReader input)
+    public override List<List<int>> ParseInputPart1(StreamReader input)
     {
-        var banks = new List<string>();
+        var banks = new List<List<int>>();
         while (!input.EndOfStream)
         {
-            banks.Add(input.ReadLine()!);
+            banks.Add(input.ReadLine()!
+                .Select(num => int.Parse(num.ToString()))
+                .ToList());
         }
         return banks;
     }
 
-    protected override int SolvePart1(List<string> parsedInput)
+    protected override int SolvePart1(List<List<int>> parsedInput)
     {
         var sum = 0;
         foreach (var bank in parsedInput)
         {
             var highestBattery = (0, 0);
-            for(var i = 0; i < bank.Length - 1; i++)
+            for(var i = 0; i < bank.Count - 1; i++)
             {
-                if (int.Parse(bank[i].ToString()) <= highestBattery.Item1) continue;
-                highestBattery.Item1 = int.Parse(bank[i].ToString());
+                if (bank[i] <= highestBattery.Item1) continue;
+                highestBattery.Item1 = bank[i];
                 highestBattery.Item2 = i;
             }
             
             var secondHighestBattery = (0, 0);
-            for(var i = highestBattery.Item2 + 1; i < bank.Length; i++)
+            for(var i = highestBattery.Item2 + 1; i < bank.Count; i++)
             {
-                if (int.Parse(bank[i].ToString()) <= secondHighestBattery.Item1) continue;
-                secondHighestBattery.Item1 = int.Parse(bank[i].ToString());
+                if (bank[i] <= secondHighestBattery.Item1) continue;
+                secondHighestBattery.Item1 = bank[i];
                 secondHighestBattery.Item2 = i;
             }
 
@@ -41,7 +43,7 @@ public class Day03 : Day<List<string>, int, List<string>, int>
         return sum;
     }
 
-    protected override List<string> ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+    protected override List<List<int>> ParseInputPart2(StreamReader input) => ParseInputPart1(input);
 
-    protected override int SolvePart2(List<string> parsedInput) => SolvePart1(parsedInput);
+    protected override int SolvePart2(List<List<int>> parsedInput) => SolvePart1(parsedInput);
 }
