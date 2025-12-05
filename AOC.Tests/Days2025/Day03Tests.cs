314a1d37fba7c2637f73ea89e9904640f44ac227
Dan Snow <danjsnow@outlook.com>
2025-12-05 10:34:47 +0000

    :sparkles: Day 03 2025 (#18)
    * :tada: Set up day 3 templates
    
    * :test_tube: Added part 1 test input
    
    * :sparkles: Solve part 1
    
    * :recycle: Parse into int list
    
    * :recycle: Make it more modular
    
    * :test_tube: Add part 2 test
    
    * :sparkles: Solve part 2
    
    * :recycle: Move private function to bottom

---
+++ AOC.Tests/Days2025/Day03Tests.cs
@@ -0,0 +1,42 @@
+using AOC.Days2025.Day03;
+using AOC.Tests.Common;
+using FluentAssertions;
+
+namespace AOC.Tests.Days2025;
+
+public class Day03Tests
+{
+    private const string TestInput = 
+        "987654321111111\n" + 
+        "811111111111119\n" +
+        "234234234234278\n" +
+        "818181911112111";
+
+    [Fact]
+    public void Day03_ParseAndSolveP1_ReturnCorrectAnswer()
+    {
+        // Arrange
+        Day03 day = new();
+        var testInputStream = MethodsForTesting.StringToStreamReader(TestInput);
+
+        // Act
+        var result = day.ParseAndSolveP1(testInputStream);
+
+        // Assert
+        result.Should().Be(357);
+    }
+
+    [Fact]
+    public void Day03_ParseAndSolveP2_ReturnCorrectAnswer()
+    {
+        // Arrange
+        Day03 day = new();
+        var testInputStream = MethodsForTesting.StringToStreamReader(TestInput);
+
+        // Act
+        var result = day.ParseAndSolveP2(testInputStream);
+
+        // Assert
+        result.Should().Be(3121910778619);
+    }
+}
---
+++ AOC/Days2025/Day03/Day03.cs
@@ -0,0 +1,75 @@
+namespace AOC.Days2025.Day03;
+
+public class Day03 : Day<List<List<int>>, long, List<List<int>>, long>
+{
+    protected override int DayNumber => 3;
+    protected override int Year => 2025;
+
+    public override List<List<int>> ParseInputPart1(StreamReader input)
+    {
+        var banks = new List<List<int>>();
+        while (!input.EndOfStream)
+        {
+            banks.Add(input.ReadLine()!
+                .Select(num => int.Parse(num.ToString()))
+                .ToList());
+        }
+        return banks;
+    }
+
+    protected override long SolvePart1(List<List<int>> parsedInput) => 
+        (from bank in parsedInput 
+            let firstDigit = GetHighestValueIndex(bank, 0, bank.Count - 1) 
+            let secondDigit = GetHighestValueIndex(bank, firstDigit.Index + 1)
+            select long.Parse($"{firstDigit.Value}{secondDigit.Value}"))
+        .Sum();
+
+    protected override List<List<int>> ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+
+    protected override long SolvePart2(List<List<int>> parsedInput)
+    {
+        var sum = (long)0;
+        foreach (var bank in parsedInput)
+        {
+            var battery = "";
+            
+            var valueIndex = new ValueIndex
+            {
+                Value = 0,
+                Index = -1
+            };
+            
+            for (var i = 11; i >= 0; i--)
+            {
+                valueIndex = GetHighestValueIndex(bank, valueIndex.Index + 1, bank.Count - i);
+                battery = $"{battery}{valueIndex.Value}";
+            }
+            sum += long.Parse(battery);
+        }
+        return sum;
+    }
+    
+    private ValueIndex GetHighestValueIndex(List<int> list, int start, int? end = null)
+    {
+        var valueIndex = new ValueIndex
+        {
+            Value = 0,
+            Index = start - 1
+        };
+        
+        for (var i = start; i < (end ?? list.Count); i++)
+        {
+            if (list[i] <= valueIndex.Value) continue;
+            valueIndex.Value = list[i];
+            valueIndex.Index = i;
+        }
+        return valueIndex;
+    }
+    
+}
+
+public record ValueIndex
+{
+    public int Value { get; set; }
+    public int Index { get; set; }
+}
