866a87ad1593b7d5d0f3de4fc8d4d1994f68eb03
Dan Snow <dan.snow@fundapps.co>
2025-12-05 22:55:12 +0000

    :recycle: Tidy up e.g. linq

--- AOC/Days2025/Day05/Day05.cs
+++ AOC/Days2025/Day05/Day05.cs
@@ -52,15 +52,15 @@ public class Day05 : Day<Inventory, int, HashSet<(ulong Lower, ulong Upper)>, ul
         return ranges;
     }
 
-    private HashSet<(ulong Lower, ulong Upper)> AddNewRange(HashSet<(ulong Lower, ulong Upper)> inventoryRanges, (ulong, ulong) newRange)
+    protected override ulong SolvePart2(HashSet<(ulong Lower, ulong Upper)> ranges) => ranges
+        .Aggregate((ulong)0, (current, range) => current + (range.Upper - range.Lower) + 1);
+    
+    private static HashSet<(ulong Lower, ulong Upper)> AddNewRange(HashSet<(ulong Lower, ulong Upper)> inventoryRanges, (ulong, ulong) newRange)
     {
         var overlappingRanges = new HashSet<(ulong Lower, ulong Upper)>();
-        foreach (var range in inventoryRanges.ToList())
+        foreach (var range in inventoryRanges.ToList().Where(range => Overlap(range, newRange)))
         {
-            if (Overlap(range, newRange))
-            {
-                overlappingRanges.Add(range);
-            }
+            overlappingRanges.Add(range);
         }
 
         foreach (var range in overlappingRanges)
@@ -81,7 +81,13 @@ public class Day05 : Day<Inventory, int, HashSet<(ulong Lower, ulong Upper)>, ul
         return inventoryRanges;
     }
 
-    private (ulong Lower, ulong Upper) GetOverlap(HashSet<(ulong Lower, ulong Upper)> overlappingRanges)
+    private static bool Overlap((ulong Lower, ulong Upper) first, (ulong Lower, ulong Upper) second) =>
+        InRange(first.Lower, second) 
+        || InRange(first.Upper, second)
+        || InRange(second.Lower, first)
+        || InRange(second.Upper, first);
+
+    private static (ulong Lower, ulong Upper) GetOverlap(HashSet<(ulong Lower, ulong Upper)> overlappingRanges)
     {
         var lower = ulong.MaxValue;
         var upper = ulong.MinValue;
@@ -98,30 +104,9 @@ public class Day05 : Day<Inventory, int, HashSet<(ulong Lower, ulong Upper)>, ul
         }
         return (lower, upper);
     }
-
-
-    protected override ulong SolvePart2(HashSet<(ulong Lower, ulong Upper)> ranges)
-    {
-        var total = (ulong)0;
-        foreach (var range in ranges)
-        {
-            total += range.Upper - range.Lower + 1;
-        }
-
-        return total;
-    }
     
-    private bool Overlap((ulong Lower, ulong Upper) first, (ulong Lower, ulong Upper) second) =>
-        InRange(first.Lower, second) 
-        || InRange(first.Upper, second)
-        || InRange(second.Lower, first)
-        || InRange(second.Upper, first);
-    
-    private bool InRange(ulong number, (ulong Lower, ulong Upper) second) =>
+    private static bool InRange(ulong number, (ulong Lower, ulong Upper) second) =>
         number >= second.Lower && number <= second.Upper;
-    
-    private (ulong Lower, ulong Upper) CreateOverlap((ulong Lower, ulong Upper) first, (ulong Lower, ulong Upper) second) =>
-        (Math.Min(first.Lower, second.Lower), Math.Max(first.Upper, second.Upper));
 }
 
 public record Inventory(HashSet<(ulong Lower, ulong Upper)> Ranges, HashSet<ulong> Ingredients);
