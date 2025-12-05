cb786b6484bbc99b7159545ca99be64a64f7d510
Dan Snow <dan.snow@fundapps.co>
2025-12-05 09:55:22 +0000

    :sparkles: Solve part 1

--- AOC/Days2025/Day03/Day03.cs
+++ AOC/Days2025/Day03/Day03.cs
@@ -1,21 +1,47 @@
 namespace AOC.Days2025.Day03;
 
-public class Day03 : Day<string, int, string, int>
+public class Day03 : Day<List<string>, int, List<string>, int>
 {
     protected override int DayNumber => 3;
     protected override int Year => 2025;
 
-    public override string ParseInputPart1(StreamReader input)
+    public override List<string> ParseInputPart1(StreamReader input)
     {
-        throw new NotImplementedException();
+        var banks = new List<string>();
+        while (!input.EndOfStream)
+        {
+            banks.Add(input.ReadLine()!);
+        }
+        return banks;
     }
 
-    protected override int SolvePart1(string parsedInput)
+    protected override int SolvePart1(List<string> parsedInput)
     {
-        throw new NotImplementedException();
+        var sum = 0;
+        foreach (var bank in parsedInput)
+        {
+            var highestBattery = (0, 0);
+            for(var i = 0; i < bank.Length - 1; i++)
+            {
+                if (int.Parse(bank[i].ToString()) <= highestBattery.Item1) continue;
+                highestBattery.Item1 = int.Parse(bank[i].ToString());
+                highestBattery.Item2 = i;
+            }
+            
+            var secondHighestBattery = (0, 0);
+            for(var i = highestBattery.Item2 + 1; i < bank.Length; i++)
+            {
+                if (int.Parse(bank[i].ToString()) <= secondHighestBattery.Item1) continue;
+                secondHighestBattery.Item1 = int.Parse(bank[i].ToString());
+                secondHighestBattery.Item2 = i;
+            }
+
+            sum += int.Parse($"{highestBattery.Item1}{secondHighestBattery.Item1}");
+        }
+        return sum;
     }
 
-    protected override string ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+    protected override List<string> ParseInputPart2(StreamReader input) => ParseInputPart1(input);
 
-    protected override int SolvePart2(string parsedInput) => SolvePart1(parsedInput);
+    protected override int SolvePart2(List<string> parsedInput) => SolvePart1(parsedInput);
 }
