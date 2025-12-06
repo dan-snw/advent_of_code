bca526696d7105456e63153170cdee1ccf150fd7
Dan Snow <dan.snow@fundapps.co>
2025-12-06 09:28:45 +0000

    :tada: Add templates

---
+++ AOC.Tests/Days2025/Day06Tests.cs
@@ -0,0 +1,38 @@
+using AOC.Days2025.Day06;
+using AOC.Tests.Common;
+using FluentAssertions;
+
+namespace AOC.Tests.Days2025;
+
+public class Day06Tests
+{
+    private const string TestInput = "";
+
+    [Fact]
+    public void Day06_ParseAndSolveP1_ReturnCorrectAnswer()
+    {
+        // Arrange
+        Day06 day = new();
+        var testInputStream = MethodsForTesting.StringToStreamReader(TestInput);
+
+        // Act
+        var result = day.ParseAndSolveP1(testInputStream);
+
+        // Assert
+        result.Should().Be(0);
+    }
+
+    [Fact]
+    public void Day06_ParseAndSolveP2_ReturnCorrectAnswer()
+    {
+        // Arrange
+        Day06 day = new();
+        var testInputStream = MethodsForTesting.StringToStreamReader(TestInput);
+
+        // Act
+        var result = day.ParseAndSolveP2(testInputStream);
+
+        // Assert
+        result.Should().Be(0);
+    }
+}
