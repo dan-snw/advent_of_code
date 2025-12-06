cff755573a66ef036306b762a97a0454ebb15058
Dan Snow <dan.snow@fundapps.co>
2025-12-06 13:36:03 +0000

    :sparkles: Hackily solve part 2

--- AOC/Days2025/Day06/Day06.cs
+++ AOC/Days2025/Day06/Day06.cs
@@ -1,6 +1,6 @@
 namespace AOC.Days2025.Day06;
 
-public class Day06 : Day<MathGrid, ulong, MathGrid, ulong>
+public class Day06 : Day<MathGrid, ulong, MathGrid2, ulong>
 {
     protected override int DayNumber => 6;
     protected override int Year => 2025;
@@ -63,12 +63,98 @@ public class Day06 : Day<MathGrid, ulong, MathGrid, ulong>
         return total;
     }
 
-    protected override MathGrid ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+    protected override MathGrid2 ParseInputPart2(StreamReader input)
+    {
+        var lines = new List<string>();
+        var symbols = new List<MathSymbol>();
+        while(!input.EndOfStream)
+        {
+            var line = input.ReadLine()!;
+            if (line.StartsWith("+") || line.StartsWith("*"))
+            {
+                symbols = line
+                    .Split(" ")
+                    .Select(s => s.Trim())
+                    .Where(s => s != "")
+                    .Select(s => s.Equals("+") ? MathSymbol.Plus : MathSymbol.Times)
+                    .Reverse()
+                    .ToList();
+                break;
+            }
 
-    protected override ulong SolvePart2(MathGrid parsedInput) => SolvePart1(parsedInput);
+            lines.Add(line);
+        }
+
+        var lineLength = lines[0].Length;
+        var i = lineLength - 1;
+        var numbers = new List<List<int>>();
+        var numbersToAdd = new List<int>();
+        while (i >= 0)
+        {
+            var allSpaces = true;
+            var number = "";
+            foreach (var line in lines)
+            {
+                if (line[i] != ' ')
+                {
+                    allSpaces = false;
+                    number += line[i];
+                }
+            }
+
+            if (allSpaces)
+            {
+                numbers.Add(numbersToAdd);
+                numbersToAdd = [];
+            }
+            else
+            {
+                numbersToAdd.Add(int.Parse(number));
+            }
+
+            if (i == 0)
+            {
+                numbers.Add(numbersToAdd);
+            }
+            i--;
+        }
+        return new MathGrid2(numbers, symbols);
+    }
+
+    protected override ulong SolvePart2(MathGrid2 parsedInput)
+    {
+        var total = (ulong)0;
+        for (var i = 0; i < parsedInput.Symbols.Count; i++)
+        {
+            if (parsedInput.Symbols[i] == MathSymbol.Plus)
+            {
+                var subTotal = (ulong)0;
+                foreach (var number in parsedInput.Numbers[i])
+                {
+                    subTotal += (ulong)number;
+                }
+
+                total += subTotal;
+            }
+            else
+            {
+                var subTotal = (ulong)1;
+                foreach (var number in parsedInput.Numbers[i])
+                {
+                    subTotal *= (ulong)number;
+                }
+
+                total += subTotal;
+            }
+        }
+
+        return total;
+    }
 }
 
 public record MathGrid(List<List<int>> NumberRows, List<MathSymbol> Symbols);
+
+public record MathGrid2(List<List<int>> Numbers, List<MathSymbol> Symbols);
 
 public enum MathSymbol
 {
