8f918488c1ad88af763df679fa5c6be25aea7d83
Dan Snow <dan.snow@fundapps.co>
2025-12-07 20:54:43 +0000

    :tada: Add templated files

---
+++ AOC/Days2025/Day07/Day07.cs
@@ -0,0 +1,21 @@
+namespace AOC.Days2025.Day07;
+
+public class Day07 : Day<string, int, string, int>
+{
+    protected override int DayNumber => 7;
+    protected override int Year => 2025;
+
+    public override string ParseInputPart1(StreamReader input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override int SolvePart1(string parsedInput)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override string ParseInputPart2(StreamReader input) => ParseInputPart1(input);
+
+    protected override int SolvePart2(string parsedInput) => SolvePart1(parsedInput);
+}
